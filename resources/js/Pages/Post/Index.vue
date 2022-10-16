4d4c9e7745cac1658b2cde4f0c91b2f03c61c9c2
aboriska <aboriska@yahoo.com>
2022-10-16 22:06:37 +0300

    Tailwind

---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto w-96 pt-8">
+        <h1 class="text-lg">Posts</h1>
+        <div>
+            <Link
+                class="hover:bg-white hover:text-sky-500 block p-2 w-32 border border-sky-500 bg-sky-500 rounded-full text-center text-white"
+                >Add Post</Link
+            >
+        </div>
+    </div>
+</template>
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    name: "Index",
+    components: {
+        Link,
+    },
+};
+</script>
+<style scoped></style>
